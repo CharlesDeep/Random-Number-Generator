16d5f17361f39b8ceb5c3f71787c26912aac0c72
CharlesDeep <weirdcesarcodes@yahoo.com>
2016-07-02 01:52:07 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Random_Number_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Generar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Generar
+            // 
+            this.Generar.Location = new System.Drawing.Point(520, 20);
+            this.Generar.Name = "Generar";
+            this.Generar.Size = new System.Drawing.Size(139, 33);
+            this.Generar.TabIndex = 0;
+            this.Generar.Text = "Generar";
+            this.Generar.UseVisualStyleBackColor = true;
+            this.Generar.Click += new System.EventHandler(this.Generar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(493, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 73);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Generar);
+            this.Name = "Form1";
+            this.Text = "Generador aleatorio de números";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Generar;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Random_Number_Generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int MinValue, MaxValue;
+        public bool trs;
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Generar_Click(object sender, EventArgs e)
+        {
+
+            Generator();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Generator()
+        {
+            try
+            {
+                Random rn = new Random();
+                int Random1 = rn.Next(1, rn.Next(-1000, 2324235));
+                int Random2 = rn.Next(rn.Next(9999, 9999999), rn.Next(1004, 578855899)) * Random1;
+                if (Random1 > Random2)
+                {
+                    MinValue = Random2;
+                    MaxValue = Random1;
+                }
+                else
+                {
+                    MinValue = Random1;
+                    MaxValue = Random2;
+                }
+                long Random3 = rn.Next(MinValue, MaxValue);
+                long Random4 = (Random3 * Random2 / Random1 * Random2) * rn.Next(int.MinValue, int.MaxValue);
+                long result = Random4;
+                textBox1.Text = result.ToString();
+            }
+            catch
+            {
+                throw new NotImplementedException();
+                trs = true;
+            }
+            finally
+            {
+                if (trs==true)
+                {
+                    this.Close();
+                }
+             }
+        }
+    }
+}
